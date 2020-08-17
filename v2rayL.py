1238a0de176a59da50e5ee5c1fa33c02a621fbb2
matteo <1434386636@qq.com>
2020-08-17 11:19:10 +0800

    test

--- v2rayL.py
+++ v2rayL.py
@@ -14,128 +14,90 @@ class V2rayL(object):
         
         try:
             with open(path, "rb") as f:
-                self.current, self.url, self.auto = pickle.load(f)
+                self.current, self.url = pickle.load(f)
         except:
             self.current = "未连接VPN"
             self.url = None
-            self.auto = False
 
         self.subs = Sub2Conf(subs_url=self.url)
 
-        if self.auto:
-            try:
-                self.subs.update()
-            except:
-                print("自动更新失败:地址错误或不存在，请更换订阅地址。")
-                self.update()
 
+    def help(self):
         print("\r------------------------------------------")
-        print("当前状态:{}".format(self.current))
-        print("自动更新:{}".format("开启" if self.auto else "关闭"))
-        print("订阅地址:{}".format(self.url if self.url else "无"))
+        print("连接：con")
+        print("断开：dcon")
+        print("配置：cfg")
+        print("订阅：sub")
+        print("状态：sta")
+        print("节点：node")
+        print("退出：exit")
+        print("帮助：help")
         print("\r------------------------------------------")
+
+    
+    def nodes(self):
+        tmp = dict()
+        num = 1
+        for k, v in self.subs.conf.items():
+            tmp[num] = k
+            num +=1
+        return tmp, num
+
+    def print_nodes(self):
+        print("{:^5}\t\t{:<20}".format("序号", "地区"))
+        tmp = self.nodes()[0]
+        for k, v in tmp.items():
+            print("{:^5}\t\t{:<20}".format(k, v))
 
         
-    def run(self):
-        print("1. 连接VPN 2. 断开VPN 3. 配置\n4. 订阅 5. 查看当前状态 0. 退出")
-        choice = input("请输入 >> ")
-
-        if choice == "1":
-            self.connect()
-        elif choice == "2":
+    def run(self, choice):
+        if choice == "con":
+            self.connect(sys.argv[2])
+        elif choice == "dcon":
             self.disconnect()
-        elif choice == "3":
-            self.cgeconf()
-        elif choice == "4":
-            self.subscribe()
-        elif choice == "5":
+        elif choice == "cfg":
+            self.cgeconf(sys.argv[2])
+        elif choice == "sub":
+            self.subscribe(sys.argv[2])
+        elif choice == "sta":
             self.status()
-
-        elif choice == "0":
+        elif choice == "node":
+            self.print_nodes()
+        elif choice == "exit":
             exit()
+        elif choice == "help":
+            self.help()
         else:
-            print("请输入正确的选项...........")
-            self.run()
+            print("argument error")
 
 
-    def cgeconf(self):
-        print("\r------------------------------------------")
-        print("1. 添加配置 2. 删除配置 0. 返回主菜单")
-        choice = input("请输入 >> ")
-        if choice == "1":
-            self.addconf()
-
-        elif choice == "2":
-            self.delconf()
-
-        elif choice == "0":
-            self.run()
-
+    def cgeconf(self, choice):
+        if choice == "add":
+            self.addconf(sys.argv[3])
+        elif choice == "del":
+            self.delconf(sys.argv[3])
         else:
-            print("请输入正确的选项:")
-            self.run()
+            print("argument error")
     
 
-    def subscribe(self):
-        print("\r------------------------------------------")
-        print("1. 开启自动更新订阅 2. 关闭自动更新订阅\n3. 修改订阅地址 0. 返回主菜单")
-        choice = input("请输入 >> ")
-        if choice == "1":
-            with open(path, "wb") as jf:
-                self.auto = True
-                pickle.dump((self.current, self.url, self.auto), jf)
-            print("已开启自动更新订阅，下次进入生效")
-            print("\r------------------------------------------")
-            self.run()
+    def subscribe(self, choice):
+        self.update(choice)
 
-        elif choice == "2":
-            with open(path, "wb") as jf:
-                self.auto = False
-                pickle.dump((self.current, self.url, self.auto), jf)
-            print("已关闭自动更新订阅，下次进入生效")
-            print("\r------------------------------------------")
-            self.run()
-
-        elif choice == "3":
-            self.update()
-
-        elif choice == "0":
-            self.run()
-        else:
-            print("请输入正确的选项:")
-            self.run()
 
     def status(self):
         print("\r------------------------------------------")
         print("当前状态: {}".format(self.current))
-        print("自动更新: {}".format("开启" if self.auto else "关闭"))
         print("订阅地址：{}".format(self.url if self.url else "无"))
         print("\r------------------------------------------")
-        self.run()
 
 
-    def connect(self):
-        print("\r------------------------------------------")
+    def connect(self, choice):
         t = self.subs.conf.items()
         if not t:
             print("无可连接的VPN，请输入订阅地址更新")
-            print("\r------------------------------------------")
-            self.run()
         else:
-            print("{:^5}\t\t{:<10}\t{:<20}".format("序号", "协议","地区"))
-            tmp = dict()
-            num = 1
-            for k, v in self.subs.conf.items():
-                print("{:^5}\t\t{:<10}\t{:<20}".format(num, v["prot"], k))
-                tmp[num] = k
-                num +=1
-
-            print("{:^5}\t\t{:<10}".format(0, "返回主菜单"))
-            choice = input("请输入 >> ")
-
-            if choice == "0":
-                self.run()
-            elif choice in [str(i) for i in range(num)]:
+            tmp, num = self.nodes()
+            if choice in [str(i) for i in range(num)]:
                 self.subs.setconf(tmp[int(choice)])
                 try:
                     print("\r正在连接")
@@ -146,19 +108,15 @@ class V2rayL(object):
                         subprocess.call(["sudo systemctl start v2ray.service"], shell=True)
                 except:
                     print("连接失败，请尝试更新订阅后再次连接")
-                    self.run()
                 else:   
                     sleep(2)
                     print("成功连接到VPN：{}".format(tmp[int(choice)]))
-                    print("\r------------------------------------------")
                     self.current = tmp[int(choice)]
                     with open(path, "wb") as jf:
-                        pickle.dump((self.current, self.url, self.auto), jf)
+                        pickle.dump((self.current, self.url), jf)
 
             else:
-                print("\r------------------------------------------")
-                print("请输入正确的选项:")
-                self.connect()
+                print("argument error")
 
 
     def disconnect(self):
@@ -169,86 +127,46 @@ class V2rayL(object):
                 subprocess.call(["sudo systemctl stop v2ray.service"], shell=True)
                 sleep(2)
                 print("VPN连接已断开")
-                print("\r------------------------------------------")
                 self.current = "未连接至VPN"
                 with open(path, "wb") as jf:
-                        pickle.dump((self.current, self.url, self.auto), jf)
+                        pickle.dump((self.current, self.url), jf)
                 
             else:
-                print("\r------------------------------------------")
                 print("服务未开启，无需断开连接")
-                print("\r------------------------------------------")
-                self.run()
         except Exception as e:
-            print("\r------------------------------------------")
             print(e)
             print("服务出错，请稍后再试")
-            print("\r------------------------------------------")
 
 
-    def update(self):
-        print("\r------------------------------------------")
-        url = input("输入 0 返回主菜单\n请输入地址>> ")
-        if url == "0":
-            self.run()
-        else:
-            print("\r正在更新订阅地址")
-            self.subs = Sub2Conf(subs_url=url)
-            self.subs.update()
-            print("订阅地址更新完成，VPN已更新")
-            print("\r------------------------------------------")
-            with open(path, "wb") as jf:
-                pickle.dump((self.current, url, self.auto), jf)
-            self.run()
+    def update(self, url):
+        print("\r正在更新订阅地址")
+        self.subs = Sub2Conf(subs_url=url)
+        self.subs.update()
+        print("订阅地址更新完成，VPN已更新")
+        with open(path, "wb") as jf:
+            pickle.dump((self.current, url), jf)
 
 
-    def addconf(self):
-        print("\r------------------------------------------")
-        url = input("请输入配置信息链接, 0返回主菜单\n(目前支持 vmess://和ss://)>>> ")
-        if url == "0":
-            self.run()
-        else:
-            print("\r正在添加配置")
-            self.subs = Sub2Conf(conf_url=url)
-            self.subs.add_conf_by_uri()
-            print("配置添加成功，VPN已更新")
-            self.run()
+    def addconf(self, url):
+        print("\r正在添加配置")
+        self.subs = Sub2Conf(conf_url=url)
+        self.subs.add_conf_by_uri()
+        print("配置添加成功，VPN已更新")
 
-    
-    def delconf(self):
-        print("\r------------------------------------------")
+        
+    def delconf(self, choice):
         t = self.subs.conf.items()
         if not t:
             print("当前无可连接VPN.")
-            print("\r------------------------------------------")
-            self.run()
         else:
-            print("{:^5}\t\t{:<10}\t{:<20}".format("序号", "协议","地区"))
-            tmp = dict()
-            num = 1
-            for k, v in self.subs.conf.items():
-                print("{:^5}\t\t{:<10}\t{:<20}".format(num, v["prot"], k))
-                tmp[num] = k
-                num +=1
-
-            print("{:^5}\t\t{:<10}".format(0, "返回主菜单"))
-            choice = input("请输入需要删除的序号 >> ")
-
-            if choice == "0":
-                self.run()
-
-            elif choice in [str(i) for i in range(num)]:
+            tmp, num = self.nodes()
+            if choice in [str(i) for i in range(num)]:
                 self.subs.delconf(tmp[int(choice)])
                 print("成功删除配置：{}".format(tmp[int(choice)]))
-                print("\r------------------------------------------")
-                self.run()
-
             else:
-                print("\r------------------------------------------")
-                print("请输入正确的选项:")
-                self.delconf()
+                print("argument error")
 
 
 if __name__ == '__main__':
     v = V2rayL()
-    v.run()
+    v.run(sys.argv[1])
--- v2rayL.sh
+++ v2rayL.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-sudo python /opt/v2rayL/v2rayL.py
+sudo python /opt/v2rayL/v2rayL.py $1 $2 $3
